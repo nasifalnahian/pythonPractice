40b98d9385227c4e4e281885dda8fe50b779dc2b
nasifalnahian <nasifalnahian393@gmail.com>
2022-04-12 16:12:08 +0600

    Initial commit

---
+++ function.py
@@ -0,0 +1,18 @@
+# first_exp = [5,6,7,8,5]
+# second_exp = [6,5,4,3,3]
+# total = 0
+# for item in  first_exp:
+#     total = total + item
+#
+#     print("first_exp total is : ",sum (first_exp))
+#     break
+# print(first_exp)
+
+tom_exp_list = [2100,3400,3500]
+joe_exp_list = [200,500,700]
+
+total=0
+for item in tom_exp_list:
+    total=total+item
+    print("tom's total expense;",total)
+    break
